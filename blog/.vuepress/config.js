99ec2063a7808cbdbd834a02b17ca57cf711e1d3
xianglong0518 <xianglong0518@126.com>
2019-11-19 23:24:06 +0800

    blog

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: 'Andy的博客随笔',
+  description: 'This is a blog example built by VuePress',
+  theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: '博客',
+        link: '/',
+      },
+      {
+        text: '标签',
+        link: '/tag/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/ulivz',
+        },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/_ulivz',
+        },
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+  },
+}
